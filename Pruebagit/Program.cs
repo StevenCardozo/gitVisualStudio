f2f514ef672dc5dcde9589640a50819c089ff978
gjh <ffvhj>
2019-08-23 20:13:31 -0400

    imprimir por consola segundo cambio

--- Pruebagit/Program.cs
+++ Pruebagit/Program.cs
@@ -11,6 +11,7 @@ namespace Pruebagit
         static void Main(string[] args)
         {
             Console.WriteLine("primer cambio");
+            Console.WriteLine("segundo cambio");
         }
     }
 }
